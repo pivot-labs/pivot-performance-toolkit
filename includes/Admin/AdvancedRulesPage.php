1c166ff3c1f3193f866cd4d870e70a2cb00cecb7
Jeffrey Shaikh <jeff@digitalcanvas.com>
2026-05-08 21:14:08 -0700

    exclusions to js minify

--- includes/Admin/AdvancedRulesPage.php
+++ includes/Admin/AdvancedRulesPage.php
@@ -57,6 +57,7 @@ final class AdvancedRulesPage implements AdminPageInterface
                 <input type="hidden" name="<?php echo esc_attr($key); ?>[minify_external_css]" value="<?php echo ! empty($options['minify_external_css']) ? '1' : '0'; ?>" />
                 <input type="hidden" name="<?php echo esc_attr($key); ?>[minify_external_css_exclusions]" value="<?php echo esc_attr((string) $options['minify_external_css_exclusions']); ?>" />
                 <input type="hidden" name="<?php echo esc_attr($key); ?>[minify_external_js]" value="<?php echo ! empty($options['minify_external_js']) ? '1' : '0'; ?>" />
+                <input type="hidden" name="<?php echo esc_attr($key); ?>[minify_external_js_exclusions]" value="<?php echo esc_attr((string) $options['minify_external_js_exclusions']); ?>" />
                 <input type="hidden" name="<?php echo esc_attr($key); ?>[minify_js]"           value="<?php echo ! empty($options['minify_js']) ? '1' : '0'; ?>" />
                 <input type="hidden" name="<?php echo esc_attr($key); ?>[defer_scripts]"       value="<?php echo ! empty($options['defer_scripts']) ? '1' : '0'; ?>" />
                 <input type="hidden" name="<?php echo esc_attr($key); ?>[lazy_load_images]"    value="<?php echo ! empty($options['lazy_load_images']) ? '1' : '0'; ?>" />
