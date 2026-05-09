4aa79ca3d6519ae1fe80ad588540e91370dd3c8a
Jeffrey Shaikh <jeff@digitalcanvas.com>
2026-05-08 21:44:36 -0700

    combine js css

--- includes/Admin/AdvancedRulesPage.php
+++ includes/Admin/AdvancedRulesPage.php
@@ -58,6 +58,10 @@ final class AdvancedRulesPage implements AdminPageInterface
                 <input type="hidden" name="<?php echo esc_attr($key); ?>[minify_external_css_exclusions]" value="<?php echo esc_attr((string) $options['minify_external_css_exclusions']); ?>" />
                 <input type="hidden" name="<?php echo esc_attr($key); ?>[minify_external_js]" value="<?php echo ! empty($options['minify_external_js']) ? '1' : '0'; ?>" />
                 <input type="hidden" name="<?php echo esc_attr($key); ?>[minify_external_js_exclusions]" value="<?php echo esc_attr((string) $options['minify_external_js_exclusions']); ?>" />
+                <input type="hidden" name="<?php echo esc_attr($key); ?>[combine_css]" value="<?php echo ! empty($options['combine_css']) ? '1' : '0'; ?>" />
+                <input type="hidden" name="<?php echo esc_attr($key); ?>[combine_css_exclusions]" value="<?php echo esc_attr((string) $options['combine_css_exclusions']); ?>" />
+                <input type="hidden" name="<?php echo esc_attr($key); ?>[combine_js]" value="<?php echo ! empty($options['combine_js']) ? '1' : '0'; ?>" />
+                <input type="hidden" name="<?php echo esc_attr($key); ?>[combine_js_exclusions]" value="<?php echo esc_attr((string) $options['combine_js_exclusions']); ?>" />
                 <input type="hidden" name="<?php echo esc_attr($key); ?>[minify_js]"           value="<?php echo ! empty($options['minify_js']) ? '1' : '0'; ?>" />
                 <input type="hidden" name="<?php echo esc_attr($key); ?>[defer_scripts]"       value="<?php echo ! empty($options['defer_scripts']) ? '1' : '0'; ?>" />
                 <input type="hidden" name="<?php echo esc_attr($key); ?>[lazy_load_images]"    value="<?php echo ! empty($options['lazy_load_images']) ? '1' : '0'; ?>" />
