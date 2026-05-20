1380eb358777599b58ae4a7378edd178364ae285
Jeffrey Shaikh <jeff@digitalcanvas.com>
2026-05-20 15:05:06 -0700

    round 1 design changes optimization page

---
+++ includes/Views/components/icons/html.blade.php
@@ -0,0 +1,12 @@
+@props([])
+
+@php
+    $svg = \PerformanceToolkit\Admin\LucideIcons::render('html');
+    $svg = str_replace('<svg ', '<svg class="h-7 w-7" ', $svg);
+@endphp
+
+<div {{ $attributes->merge(array('class' => 'flex items-center justify-center border border-orange-200 bg-orange-50 text-orange-600', 'style' => 'width: 38px; height: 38px; padding: 5px; box-sizing: border-box; border-radius: 5px;')) }}>
+    {!! $svg !!}
+</div>
+
+
