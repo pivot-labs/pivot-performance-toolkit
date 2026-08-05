cf1e451e5ebb96f882bd1707e89551f5702cbf79
Jeffrey Shaikh <jeff@digitalcanvas.com>
2026-08-05 15:10:13 -0700

    remove network config

--- pivot-performance-toolkit.php
+++ pivot-performance-toolkit.php
@@ -12,7 +12,6 @@
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: pivot-performance-toolkit
  * Domain Path: /languages
- * Network: false
  *
  * @package PivotPerformanceToolkit
  */
